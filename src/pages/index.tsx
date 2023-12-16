e2986ca9e018542e081169a32263185990953c7c
Azevedoooo <a85217@ualg.pt>
2023-12-16 19:05:46 +0000

    Elimada a pasta tutorial-extras

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/documentacao">
             Ver Documentação - 5min ⏱️
           </Link>
         </div>
